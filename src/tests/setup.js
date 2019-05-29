1d0c2c400521413e92e5469a3fdfb04ab107d86d
Satyam <satyam@satyam.com.ar>
2019-05-29 14:48:24 +0200

    Jest setup now copies original test data filesso that they can be freely trashed in tests

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
+import { join } from 'path';
+import { copy } from 'fs-extra';
 
 dotenv.config({ path: '.env.tests' });
 dotenv.config();
@@ -6,4 +8,16 @@ dotenv.config();
 // eslint-disable-next-line import/first
 import { start } from '../server';
 
-module.exports = () => start();
+function relPath(path) {
+  return join(process.cwd(), path);
+}
+
+module.exports = () =>
+  copy(relPath('src/tests/data.orig.json'), relPath(process.env.JSON_FILE))
+    .then(() =>
+      copy(
+        relPath('src/tests/db.orig.sqlite'),
+        relPath(process.env.SQLITE_FILE)
+      )
+    )
+    .then(() => start());
