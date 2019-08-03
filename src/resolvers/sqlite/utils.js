2fc0a665aaf54bf2a9f986e0fbb5b66c24b6befc
Satyam <satyam@satyam.com.ar>
2019-08-03 14:10:53 +0200

    Added a `delay` utility function to test delayed responses and specially UI 'loading...'' components

--- src/resolvers/sqlite/utils.js
+++ src/resolvers/sqlite/utils.js
@@ -66,3 +66,10 @@ export function deleteById(table, id, db, outFields) {
     })
   );
 }
+
+export function delay(ms) {
+  return value =>
+    new Promise(resolve => {
+      setTimeout(() => resolve(value), ms);
+    });
+}
